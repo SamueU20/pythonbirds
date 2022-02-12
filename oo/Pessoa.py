e9a02e9d3e344ba62061cff7844f460bcfd52069
Samuel <samuelsilva202019@gmail.com>
2022-02-11 21:12:28 -0300

    Criado e resovido atributo dinâmico de objetos de tipo Pessoa

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -1,17 +1,21 @@
 class Pessoa:
-     def __init__(self, nome = None, idade=23):
+     def __init__(self, *filhos, nome = None, idade=23):
           self.idade = idade
           self.nome = nome
+          self.filhos = list(filhos)
 
-       def cumprimetar(self):
+     def cumprimentar(self):
          return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-     p = Pessoa('Luciano')
-     print(Pessoa.cumprimentar(p))
-     print(id(p))
-     print(p.cumprimentar())
-     print(p.nome)
-     p.nome = 'samuel'
-     print(p.idade)
+     samuel = Pessoa(nome='Samuel')
+     luciano = Pessoa(samuel, nome='Luciano')
+     print(Pessoa.cumprimentar(luciano))
+     print(id(luciano))
+     print(luciano.cumprimentar())
+     print(luciano.nome)
+     print(luciano.idade)
+     for filho in luciano.filhos:
+         print(filho.nome)
+
